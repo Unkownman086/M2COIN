be9041846dd16ce93836f95ef30a4f93937cb7f9
Unkownman086 <37930275+Unkownman086@users.noreply.github.com>
2019-09-14 09:46:24 +0200

    Update CryptoNoteConfig.h

--- src/CryptoNoteConfig.h
+++ src/CryptoNoteConfig.h
@@ -82,6 +82,8 @@ const size_t   FUSION_TX_MIN_IN_OUT_COUNT_RATIO              = 4;
 const uint32_t KEY_IMAGE_CHECKING_BLOCK_INDEX                = 0;
 const uint32_t UPGRADE_HEIGHT_V2                                = 1;
 const uint32_t UPGRADE_HEIGHT_V3                                = 30;
+const uint32_t UPGRADE_HEIGHT_V4                             = 338000000000000;
+const uint8_t  BLOCK_MAJOR_VERSION_4                         =  4;
 const unsigned UPGRADE_VOTING_THRESHOLD                      = 90;               // percent
 const uint32_t UPGRADE_VOTING_WINDOW                         = EXPECTED_NUMBER_OF_BLOCKS_PER_DAY;  // blocks
 const uint32_t UPGRADE_WINDOW                                = EXPECTED_NUMBER_OF_BLOCKS_PER_DAY;  // blocks
@@ -129,11 +131,16 @@ const uint64_t P2P_DEFAULT_INVOKE_TIMEOUT                    = 60 * 2 * 1000; //
 const size_t   P2P_DEFAULT_HANDSHAKE_INVOKE_TIMEOUT          = 5000;          // 5 seconds
 const char     P2P_STAT_TRUSTED_PUB_KEY[]                    = "";
 
-const char* const SEED_NODES[] = { "10.0.0.3:21397", "10.0.0.196:21397", "10.0.0.195:21397" };
+const std::initializer_list<const char*> SEED_NODES = {
+  "10.0.0.3:21397",
+  "10.0.0.196:21397",
+  "10.0.0.195:21397",
+};
 
 
 struct CheckpointData {
   uint32_t index;
+  uint32_t height;
   const char* blockId;
 };
 
